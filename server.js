b7baca13963ac3635b56f9ab131e0c0b31c3764e
uchumi <uchumidigital@gmail.com>
2026-09-21 12:49:26 +0300

    Add announcements router to server configuration

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const ordersRouter = require('./routes/orders');
 const adminRouter = require('./routes/admin');
 const branchesRouter = require('./routes/branches');
 const promoBannersRouter = require('./routes/promo-banners');
+const announcementsRouter = require('./routes/announcements');
 const { router: customerAuthRouter, attachCustomerIfPresent } = require('./routes/customer-auth');
 
 const app = express();
@@ -32,6 +33,7 @@ app.use('/api/orders', ordersRouter);
 app.use('/api/admin', adminRouter);
 app.use('/api/branches', branchesRouter);
 app.use('/api/promo-banners', promoBannersRouter);
+app.use('/api/announcements', announcementsRouter);
 app.use('/api/customer-auth', customerAuthRouter);
 
 // --- M-Pesa routes go here once credentials are confirmed reusable ---
